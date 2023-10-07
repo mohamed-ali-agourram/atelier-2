05420768c20e641c9799a1ca849570e90470a0f7
Mohamed Ali Agourram <95295909+mohamed-ali-agourram@users.noreply.github.com>
2023-10-07 14:09:57 +0100

    fix styles

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -2,9 +2,11 @@ const Pizza = ({ pizza }) => {
  return (
   <div className={pizza.soldOut ? "pizza sold-out" : "pizza"}>
    <img src={pizza.photoName} alt={pizza.name + "_img"} />
-   <p>{pizza.name}</p>
-   <p>{pizza.price}DHs</p>
-   {pizza.soldOut && <span>Sold Out</span>}
+   <div>
+    <h3>{pizza.name}</h3>
+    <p>{pizza.price}DHs</p>
+    {pizza.soldOut && <span>Sold Out</span>}
+   </div>
   </div>
  );
 };
